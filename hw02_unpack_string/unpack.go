759392633139d7d8f701a5f2872a8285d76fdd92
lomoval <64064940+lomoval@users.noreply.github.com>
2021-09-09 00:38:28 +0300

    Add 02 homework - unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,79 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+const (
+	ignoredRune = rune(-1)
+	escapeRune  = rune('\\')
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	runes := []rune(s)
+	length := len(runes)
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
+	prevRune := ignoredRune
+	var escaped bool
+	var err error
+	b := strings.Builder{}
+	for i := 0; i < length; i++ {
+		if prevRune, escaped, err = processRune(runes[i], prevRune, escaped, &b); err != nil {
+			return "", err
+		}
+	}
+	if prevRune == escapeRune && !escaped {
+		return "", ErrInvalidString
+	}
+	if !unicode.IsDigit(prevRune) || escaped {
+		b.WriteRune(prevRune)
+	}
+
+	return b.String(), nil
+}
+
+func processRune(cur rune, prev rune, escaped bool, builder *strings.Builder) (rune, bool, error) {
+	if unicode.IsDigit(prev) && !escaped && unicode.IsDigit(cur) {
+		return cur, false, ErrInvalidString
+	}
+	if prev == escapeRune && (cur != escapeRune && !unicode.IsDigit(cur)) {
+		return ignoredRune, false, ErrInvalidString
+	}
+	if !escaped && unicode.IsDigit(prev) {
+		prev = ignoredRune
+	}
+
+	escaped = !escaped && prev == escapeRune
+	if escaped && cur == escapeRune {
+		prev = ignoredRune
+	}
+
+	if unicode.IsDigit(cur) {
+		if escaped {
+			return cur, escaped, nil
+		}
+		count, err := strconv.Atoi(string(cur))
+		if err != nil {
+			return rune(0), false, err
+		}
+		builder.WriteString(strings.Repeat(string(prev), count))
+		return cur, escaped, nil
+	}
+
+	if prev != ignoredRune {
+		builder.WriteRune(prev)
+	}
+
+	return cur, escaped, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -3,6 +3,7 @@ package hw02unpackstring
 import (
 	"errors"
 	"testing"
+	"unicode"
 
 	"github.com/stretchr/testify/require"
 )
@@ -16,11 +17,46 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+
+		{input: string(rune(-1)), expected: "�"},
+		{input: string(rune(-10)) + "2", expected: "��"},
+		{input: string(rune(-100)) + `\2`, expected: "�2"},
+		{input: "\u0000", expected: "\u0000"},
+		{input: "\u00002", expected: "\u0000\u0000"},
+		{input: "\u7777", expected: "\u7777"},
+		{input: "\u77772", expected: "\u7777\u7777"},
+		{input: `\\`, expected: `\`},
+		{input: `\1`, expected: `1`},
+		{input: `\2\3`, expected: `23`},
+		{input: `\23a`, expected: `222a`},
+		{input: `T2\2`, expected: `TT2`},
+		{input: `#2!2$2`, expected: `##!!$$`},
+		{input: `\\\\\\2`, expected: `\\\\`},
+		{input: ` 2qwe\\\3`, expected: `  qwe\3`},
+		{input: "  ", expected: "  "},
+		{input: ` 1`, expected: ` `},
+		{input: ` 2`, expected: `  `},
+		{input: ` 0`, expected: ``},
+		{input: ` 2 2 2`, expected: `      `},
+		{input: `\1 2 2 2\1`, expected: `1      1`},
+		{input: `qwe\3`, expected: `qwe3`},
+		{input: `qwe\30`, expected: `qwe`},
+		{input: `a0`, expected: ``},
+		{input: `\00`, expected: ``},
+		{input: `\10`, expected: ``},
+		{input: `\0\0`, expected: `00`},
+		{input: `a\0`, expected: `a0`},
+		{input: `aa0`, expected: `a`},
+		{input: `a1b1c1`, expected: `abc`},
+		{input: `в1Ы1ф1`, expected: `вЫф`},
+		{input: `-1`, expected: `-`},
+		{input: `a-1`, expected: `a-`},
 	}
 
 	for _, tc := range tests {
@@ -34,12 +70,22 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{`\0x000`, `qw\ne`, `0\0`, "3abc", "00", "45", "aaa10b", `\`, `\w`, `www\`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
+	}
+}
+
+func TestUnpackInvalidEscape(t *testing.T) {
+	for i := 0; i < 255; i++ {
+		if rune(i) == '\\' || unicode.IsDigit(rune(i)) {
+			continue
+		}
+		_, err := Unpack(`\` + string(rune(i)))
+		require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 	}
 }
