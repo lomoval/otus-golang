212cb406e27e5a924810ea5387113b72ee23c303
lomoval <64064940+lomoval@users.noreply.github.com>
2021-11-26 13:13:40 +0300

    Add 09 homework - structure validator

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,35 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	tagNameValidate = "validate"
+	tagValueNested  = "nested"
+	tagValueIn      = "in"
+	tagValueMax     = "max"
+	tagValueMin     = "min"
+	tagValueLen     = "len"
+	tagValueRegexp  = "regexp"
+)
+
+var (
+	ErrIncorrectTagValue        = errors.New("incorrect tag value for validating with field value")
+	ErrValidateIncorrectLen     = errors.New("value has incorrect length")
+	ErrValidateNotMatchRegexp   = errors.New("does not match regexp")
+	ErrValidateNotFoundInList   = errors.New("does not found in list")
+	ErrValidateIncorrectNumeric = errors.New("incorrect numeric value")
+	ErrIncorrectTag             = errors.New("incorrect tag")
+	ErrIncorrectStruct          = errors.New("incorrect struct")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -7,11 +37,338 @@ type ValidationError struct {
 
 type ValidationErrors []ValidationError
 
+type validationData struct {
+	Name            string
+	SourceValue     string
+	ConvertedValues interface{}
+}
+
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	sort.Slice(v, func(i, j int) bool {
+		if v[i].Field == v[j].Field {
+			return v[i].Err.Error() < v[j].Err.Error()
+		}
+		return v[i].Field < v[j].Field
+	})
+	b := strings.Builder{}
+	for _, validationError := range v {
+		b.WriteString(fmt.Sprintf("{name: %s, error: %s}", validationError.Field, validationError.Err.Error()))
+	}
+	return b.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
-	return nil
+	if v == nil {
+		return ErrIncorrectStruct
+	}
+	rv := reflect.ValueOf(v)
+	t := reflect.TypeOf(v)
+	if t.Kind() != reflect.Struct {
+		return ErrIncorrectStruct
+	}
+
+	var validatorErrors ValidationErrors
+	for i := 0; i < rv.NumField(); i++ {
+		field := rv.Field(i)
+		validationData, err := parseValidateTag(field, t.Field(i).Tag)
+		if err != nil {
+			return err
+		}
+		if len(validationData) == 0 {
+			continue
+		}
+
+		if validationData[0].Name == tagValueNested {
+			err = Validate(field.Interface())
+			var vErrors ValidationErrors
+			if errors.As(err, &vErrors) {
+				validatorErrors = append(validatorErrors, vErrors...)
+				continue
+			}
+			return err
+		}
+
+		validatorErrors, err = validateField(validationData, field, t.Field(i).Name, validatorErrors)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(validatorErrors) == 0 {
+		return nil
+	}
+	return validatorErrors
+}
+
+func validateField(
+	validationData []validationData,
+	field reflect.Value,
+	fieldName string,
+	validatorErrors ValidationErrors,
+) (ValidationErrors, error) {
+	var err error
+
+	if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
+		for _, data := range validationData {
+			validatorErrors, err = validateSlice(field, fieldName, data, validatorErrors)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return validatorErrors, nil
+	}
+
+	for _, data := range validationData {
+		if validatorErrors, err = validateValue(
+			fieldName,
+			field.Interface(),
+			field.Type(),
+			data,
+			validatorErrors,
+		); err != nil {
+			return nil, err
+		}
+	}
+	return validatorErrors, nil
+}
+
+func validateSlice(
+	field reflect.Value,
+	fieldName string,
+	data validationData,
+	validatorErrors ValidationErrors,
+) (ValidationErrors, error) {
+	var err error
+	for f := 0; f < field.Len(); f++ {
+		validatorErrors, err = validateValue(
+			fieldName,
+			field.Index(f).Interface(),
+			field.Type().Elem(),
+			data,
+			validatorErrors,
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return validatorErrors, nil
+}
+
+func validateValue(
+	fieldName string,
+	val interface{},
+	t reflect.Type,
+	data validationData,
+	validationErrors ValidationErrors,
+) (ValidationErrors, error) {
+	if t.Kind() == reflect.String {
+		var err error
+		validationErrors, err = validateStringValue(fieldName, val, data, validationErrors)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if data.Name == tagValueIn {
+		if !inSlice(val, data.ConvertedValues) {
+			validationErrors = append(validationErrors, ValidationError{Field: fieldName, Err: ErrValidateNotFoundInList})
+		}
+		return validationErrors, nil
+	}
+
+	if data.Name == tagValueMin {
+		if !ge(val, reflect.ValueOf(data.ConvertedValues).Index(0).Interface()) {
+			validationErrors = append(validationErrors, ValidationError{Field: fieldName, Err: ErrValidateIncorrectNumeric})
+		}
+		return validationErrors, nil
+	}
+
+	if data.Name == tagValueMax {
+		if !le(val, reflect.ValueOf(data.ConvertedValues).Index(0).Interface()) {
+			validationErrors = append(validationErrors, ValidationError{Field: fieldName, Err: ErrValidateIncorrectNumeric})
+		}
+		return validationErrors, nil
+	}
+	return validationErrors, nil
+}
+
+func validateStringValue(
+	fieldName string,
+	val interface{},
+	data validationData,
+	validationErrors ValidationErrors,
+) (ValidationErrors, error) {
+	if data.Name == tagValueLen {
+		check, err := strconv.Atoi(data.SourceValue)
+		if err != nil {
+			return nil, ErrIncorrectTagValue
+		}
+		if len(val.(string)) != check {
+			validationErrors = append(validationErrors, ValidationError{
+				Field: fieldName,
+				Err:   ErrValidateIncorrectLen,
+			})
+		}
+		return validationErrors, nil
+	}
+
+	if data.Name == tagValueRegexp {
+		r, err := regexp.Compile(data.SourceValue)
+		if err != nil {
+			return nil, ErrIncorrectTagValue
+		}
+		match := r.FindString(val.(string))
+		if len(match) != len(val.(string)) {
+			return append(validationErrors, ValidationError{Field: fieldName, Err: ErrValidateNotMatchRegexp}), nil
+		}
+	}
+
+	return validationErrors, nil
+}
+
+func parseValidateTag(field reflect.Value, tag reflect.StructTag) ([]validationData, error) {
+	val := tag.Get(tagNameValidate)
+	if val == "" {
+		return nil, nil
+	}
+	validators := strings.Split(val, "|")
+
+	data := make([]validationData, 0, len(validators))
+	for _, validator := range validators {
+		validatorParts := strings.SplitN(validator, ":", 2)
+		if len(validatorParts) == 1 {
+			if validatorParts[0] != tagValueNested {
+				return nil, ErrIncorrectTag
+			}
+			// Ignore other validators if nested
+			return []validationData{{Name: validatorParts[0]}}, nil
+		}
+
+		validatorValues := []string{validatorParts[1]}
+		if validatorParts[0] == tagValueIn {
+			validatorValues = strings.Split(validatorParts[1], ",")
+		}
+
+		fieldType := field.Type()
+		if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
+			fieldType = field.Type().Elem()
+		}
+
+		values, err := convertSliceByType(validatorValues, fieldType)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, validationData{Name: validatorParts[0], ConvertedValues: values, SourceValue: validatorParts[1]})
+	}
+	return data, nil
+}
+
+func convertSliceByType(values []string, t reflect.Type) (interface{}, error) {
+	slice := reflect.MakeSlice(reflect.SliceOf(t), 0, len(values))
+	size := (int)(t.Size()) * 8
+	var convert func(v string) (interface{}, error)
+
+	// Not planning to implement converters for all types
+	//exhaustive:ignore
+	switch t.Kind() {
+	case reflect.String:
+		if t.String() == "string" {
+			return values, nil
+		}
+		convert = func(str string) (interface{}, error) {
+			productPointer := reflect.New(t).Elem()
+			productPointer.SetString(str)
+			return productPointer.Interface(), nil
+		}
+	case reflect.Int8:
+		convert = func(str string) (interface{}, error) {
+			val, err := strconv.ParseInt(str, 0, size)
+			return (int8)(val), err
+		}
+	case reflect.Int16:
+		convert = func(str string) (interface{}, error) {
+			val, err := strconv.ParseInt(str, 0, size)
+			return (int16)(val), err
+		}
+	case reflect.Int32:
+		convert = func(str string) (interface{}, error) {
+			val, err := strconv.ParseInt(str, 0, size)
+			return (int32)(val), err
+		}
+	case reflect.Int64:
+		convert = func(str string) (interface{}, error) {
+			return strconv.ParseInt(str, 0, size)
+		}
+	case reflect.Float32:
+		convert = func(str string) (interface{}, error) {
+			val, err := strconv.ParseFloat(str, size)
+			return (float32)(val), err
+		}
+	case reflect.Float64:
+		convert = func(str string) (interface{}, error) {
+			return strconv.ParseFloat(str, size)
+		}
+	default:
+		convert = func(str string) (interface{}, error) {
+			val, err := strconv.ParseInt(str, 0, size)
+			return (int)(val), err
+		}
+	}
+
+	for _, val := range values {
+		v, err := convert(val)
+		if err != nil {
+			return nil, ErrIncorrectTagValue
+		}
+		slice = reflect.Append(slice, reflect.ValueOf(v))
+	}
+	return slice.Interface(), nil
+}
+
+func inSlice(v interface{}, in interface{}) bool {
+	s := reflect.ValueOf(in)
+	for i := 0; i < s.Len(); i++ {
+		if v == s.Index(i).Interface() {
+			return true
+		}
+	}
+	return false
+}
+
+func le(a interface{}, b interface{}) bool {
+	switch v := a.(type) {
+	case int8:
+		return v <= b.(int8)
+	case int16:
+		return v <= b.(int16)
+	case int32:
+		return v <= b.(int32)
+	case int64:
+		return v <= b.(int64)
+	case float32:
+		return v <= b.(float32)
+	case float64:
+		return v <= b.(float64)
+	default:
+		return a.(int) <= b.(int)
+	}
+}
+
+func ge(a interface{}, b interface{}) bool {
+	switch v := a.(type) {
+	case int8:
+		return v >= b.(int8)
+	case int16:
+		return v >= b.(int16)
+	case int32:
+		return v >= b.(int32)
+	case int64:
+		return v >= b.(int64)
+	case float32:
+		return v >= b.(float32)
+	case float64:
+		return v >= b.(float64)
+	default:
+		return a.(int) >= b.(int)
+	}
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -24,6 +26,15 @@ type (
 		Version string `validate:"len:5"`
 	}
 
+	Regexp struct {
+		Str1 string `validate:"regexp:\\d+|len:5"`
+	}
+
+	Float struct {
+		F1 float32 `validate:"min:5.5"`
+		F2 float64 `validate:"min:5.5|max:6.6"`
+	}
+
 	Token struct {
 		Header    []byte
 		Payload   []byte
@@ -34,18 +45,85 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	Nested struct {
+		Response `validate:"nested"`
+		User     `validate:"nested"`
+		Num      int8 `validate:"min:5|max:6"`
+	}
 )
 
-func TestValidate(t *testing.T) {
+func TestValidateCorrectValues(t *testing.T) {
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in:          App{Version: "1.0.0"},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: Nested{
+				Response: Response{Code: 500, Body: ""},
+				User: User{
+					ID:     "e6bb694e-0669-4ad6-9920-41162213e92d",
+					Name:   "",
+					Age:    36,
+					Email:  "test1@test.ru",
+					Role:   "stuff",
+					Phones: []string{"12345678901", "09876543210"},
+				},
+				Num: 5,
+			},
+			expectedErr: nil,
+		},
+		{
+			in: User{
+				ID:     "e6bb694e-0669-4ad6-9920-41162213e92d",
+				Name:   "",
+				Age:    18,
+				Email:  "test@test.ru",
+				Role:   "stuff",
+				Phones: []string{"12345678901", "09876543210"},
+				meta:   []byte{1},
+			},
+			expectedErr: nil,
+		},
+		{
+			in: User{
+				ID:     "e6bb694e-0669-4ad6-9920-41162213e92d",
+				Name:   "",
+				Age:    50,
+				Email:  "test@test.ru",
+				Role:   "admin",
+				Phones: []string{"12345678901", "09876543210"},
+				meta:   []byte{1},
+			},
+			expectedErr: nil,
+		},
+		{
+			in:          Response{Code: 404, Body: ""},
+			expectedErr: nil,
+		},
+		{
+			in: Token{
+				Header:    []byte{1},
+				Payload:   []byte{1, 2},
+				Signature: []byte{1, 2, 3},
+			},
+			expectedErr: nil,
+		},
+		{
+			in:          Float{F1: 5.5, F2: 6.1},
+			expectedErr: nil,
+		},
+		{
+			in:          Float{F1: 15.5, F2: 6.6},
+			expectedErr: nil,
+		},
+		{
+			in: Regexp{Str1: "12345"},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +131,155 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+
+			require.ErrorIs(t, err, tt.expectedErr)
+		})
+	}
+}
+
+func TestValidateIncorrectValues(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in: App{Version: "1.0"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Version", Err: ErrValidateIncorrectLen},
+			},
+		},
+		{
+			in: User{
+				ID:     "1",
+				Name:   "testName",
+				Age:    5,
+				Email:  "incorrect-email",
+				Role:   "user",
+				Phones: []string{"1234567890"},
+				meta:   []byte{1},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrValidateIncorrectLen},
+				ValidationError{Field: "Age", Err: ErrValidateIncorrectNumeric},
+				ValidationError{Field: "Email", Err: ErrValidateNotMatchRegexp},
+				ValidationError{Field: "Role", Err: ErrValidateNotFoundInList},
+				ValidationError{Field: "Phones", Err: ErrValidateIncorrectLen},
+			},
+		},
+		{
+			in: Response{Code: 0, Body: ""},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Code", Err: ErrValidateNotFoundInList},
+			},
+		},
+		{
+			in: Float{F1: 5.4, F2: 6.1},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "F1", Err: ErrValidateIncorrectNumeric},
+			},
+		},
+		{
+			in: Float{F1: 15.5, F2: 6.7},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "F2", Err: ErrValidateIncorrectNumeric},
+			},
+		},
+		{
+			in: Regexp{Str1: "123456"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Str1", Err: ErrValidateIncorrectLen},
+			},
+		},
+		{
+			in: Regexp{Str1: "12a45"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Str1", Err: ErrValidateNotMatchRegexp},
+			},
+		},
+		{
+			in: Regexp{Str1: "123a456"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Str1", Err: ErrValidateIncorrectLen},
+				ValidationError{Field: "Str1", Err: ErrValidateNotMatchRegexp},
+			},
+		},
+		{
+			in: Nested{
+				Response: Response{Code: 600, Body: ""},
+				User: User{
+					ID:     "6bb694e-0669-4ad6-9920-41162213e92d",
+					Name:   "",
+					Age:    51,
+					Email:  "test1test.ru",
+					Role:   "no",
+					Phones: []string{"1234568901", "098765432"},
+				},
+				Num: 10,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrValidateIncorrectLen},
+				ValidationError{Field: "Age", Err: ErrValidateIncorrectNumeric},
+				ValidationError{Field: "Email", Err: ErrValidateNotMatchRegexp},
+				ValidationError{Field: "Role", Err: ErrValidateNotFoundInList},
+				ValidationError{Field: "Phones", Err: ErrValidateIncorrectLen},
+				ValidationError{Field: "Phones", Err: ErrValidateIncorrectLen},
+				ValidationError{Field: "Code", Err: ErrValidateNotFoundInList},
+				ValidationError{Field: "Num", Err: ErrValidateIncorrectNumeric},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+
+			require.EqualError(t, tt.expectedErr, err.Error())
+		})
+	}
+}
+
+func TestValidateIncorrectInput(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in:          nil,
+			expectedErr: ErrIncorrectStruct,
+		},
+		{
+			in:          1,
+			expectedErr: ErrIncorrectStruct,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+
+			require.ErrorIs(t, tt.expectedErr, err)
 		})
 	}
 }
