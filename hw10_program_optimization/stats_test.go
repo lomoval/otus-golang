a5396250b09fd1a8b87a860c0479e60406901243
lomoval <64064940+lomoval@users.noreply.github.com>
2021-12-03 21:39:16 +0300

    Add 10 homework - program optimization
    Benchmarks:
    ```
    name     old time/op    new time/op    delta
    Basic-4     296ms ± 3%       0ms ± 8%  -99.99%  (p=0.000 n=13+10)
    
    name     old alloc/op   new alloc/op   delta
    Basic-4     133MB ± 0%       0MB ± 0%  -99.99%  (p=0.000 n=13+13)
    
    name     old allocs/op  new allocs/op  delta
    Basic-4     1.70M ± 0%     0.00M ± 0%  -99.99%  (p=0.000 n=13+13)
    ```

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -37,3 +37,53 @@ func TestGetDomainStat(t *testing.T) {
 		require.Equal(t, DomainStat{}, result)
 	})
 }
+
+func TestIncorrectData(t *testing.T) {
+	t.Run("empty email", func(t *testing.T) {
+		data := `{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("no email", func(t *testing.T) {
+		data := `{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("incorrect email", func(t *testing.T) {
+		data := `{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"incorrect<dog>email.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+
+	t.Run("incorrect json", func(t *testing.T) {
+		data := `{"Id":2,"Name""Jesse Vasquez","Username":"qRichardson","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+}
+
+func BenchmarkBasic(b *testing.B) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}
+{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	for i := 0; i < b.N; i++ {
+		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
